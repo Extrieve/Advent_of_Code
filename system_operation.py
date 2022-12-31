96ae3527216a15c6555c7d9930feb32dc8fc4c55
Extrieve <nicolas.cinera@gmail.com>
2022-12-31 11:31:22 -0500

    Refactoring

--- system_operation.py
+++ system_operation.py
@@ -1,14 +1,19 @@
 import os
 import shutil
 
-def get_file_list(path):
+def get_file_list(path, keyword) -> list:
     """Return a list of all files in a directory."""
     # if file contains the word 'day' move it to 2022
+    files = []
     for file in os.listdir(path):
-        if 'day' in file:
-            print(file)
+        if keyword in file: 
             shutil.move(file, '2022')
+            files.append(file)
+    return files
 
+def main() -> None:
+    """Run the main function."""
+    get_file_list('.', 'day')
 
 if __name__ == '__main__':
-    get_file_list('.')
+    main()
