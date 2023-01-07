4f04068fc3c71300f81ad7d5a08c7d07e739a8df
Extrieve <nicolas.cinera@gmail.com>
2023-01-07 09:49:07 -0500

    Day 2 2021 -> DCC

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -1,15 +1,19 @@
 with open('2021/input.txt', 'r') as f:
     data = f.read().split('\n')
 
-x, y = 0, 0
+x, y, aim = 0, 0, 0
 
 for line in data:
     command, moves = line.split(' ')
+    moves = int(moves)
     if 'forward' in command:
-        x += int(moves)
+        x += moves
+        y += moves * aim
     elif 'down' in command:
-        y += int(moves)
+        aim += moves
+        # y += moves
     else:
-        y -= int(moves)
+        aim -= moves
+        # y -= moves
 
 print(x, y, x * y)
